a156a583ddc38baa2d8b6ecf39d6e705c4bed2f8
srishanmsc <114826495+srishanmsc@users.noreply.github.com>
2022-10-13 03:48:39 -0700

    Add files via upload

---
+++ day1/Learnmetho.java
@@ -0,0 +1,14 @@
+package week.day1;
+import week1.day2.ClassArthemetic;
+
+public class Learnmetho {
+public static void main(String[] args) {
+	Learnmetho a=new Learnmetho();
+	int output= a.Addition(1,2);
+	System.out.println(output);
+	int output1=a.Sub(3, 2);
+	System.out.println(output1);
+	int ouput3=a.Mul(2,2,3);
+	System.out.println(ouput3);
+}
+}
